3e1f78fe8290628877098adb2143a053218c3618
Jacob <110312077+Jacob59569@users.noreply.github.com>
2024-08-31 15:31:49 +0300

    Add main.go and add web server to main.go

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
-package go_micro_shop
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler) // Route to the handler function
+
+	fmt.Println("Starting server on :8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
+}
